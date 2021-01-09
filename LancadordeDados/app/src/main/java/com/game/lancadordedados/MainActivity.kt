18b7ea18fe5f40ecd25e627c19f0a28602c7f5ee
Jeff Araujo <jefferson_lord@yahoo.com.br>
2021-01-09 17:36:17 -0300

    finalizando o projeto com inclusão do dark/light mode

--- LancadordeDados/app/src/main/java/com/game/lancadordedados/MainActivity.kt
+++ LancadordeDados/app/src/main/java/com/game/lancadordedados/MainActivity.kt
@@ -3,18 +3,45 @@ package com.game.lancadordedados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // apertar o botão, para lançar o dado 1 e 2
-        findViewById<Button>(R.id.buttonJogar).setOnClickListener {
-            
+        val botaoLancarDados = findViewById<Button>(R.id.buttonLancar)
+        val imgDado1 = findViewById<ImageView>(R.id.imageDado1)
+        val imgDado2 = findViewById<ImageView>(R.id.imageDado2)
+
+        botaoLancarDados.setOnClickListener {
+            imgDado1.setImageResource(lancaDado())
+            imgDado2.setImageResource(lancaDado())
+        }
+
+        val nomeJogador = getIntent().getStringExtra("nomeDoJogador")
+        val nomeNaTela = findViewById<TextView>(R.id.textoNome)
+        nomeNaTela.text = "Olá, $nomeJogador"
+    }
+
+    fun lancaDado(): Int {
+        return recuperaFaceDado(gerarNumero())
+    }
+
+
+    fun recuperaFaceDado(valor: Int): Int {
+        return when(valor) {
+            1 -> R.drawable.dado1
+            2 -> R.drawable.dado2
+            3 -> R.drawable.dado3
+            4 -> R.drawable.dado4
+            5 -> R.drawable.dado5
+            else -> R.drawable.dado6
         }
     }
-    fun lancarDados(): Int {
+
+    fun gerarNumero(): Int {
         return (1..6).random()
     }
 }
